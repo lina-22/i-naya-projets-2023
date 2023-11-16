97cd270edbe9592a7961a2fc943584c5e4acaea4
lina-22 <haquefahinur@gmail.com>
2023-11-16 12:07:44 +0100

    init web socket project

---
+++ spring-boot-starter-websocket/spring-boot-starter-websocket/src/main/java/org/springframework/boot/springbootstarterwebsocket/SpringBootStarterWebsocketApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.springbootstarterwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterWebsocketApplication.class, args);
+	}
+
+}
