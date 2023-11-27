1247d11e064ec36fe78a64db98bfa7fe0a8b82cf
lina-22 <haquefahinur@gmail.com>
2023-11-27 02:31:48 +0100

    Angular material Autocomplete | load data from API & Filtering |Angular 15 - Material UI Tutorial

--- tpAngulaMaterialUI/src/app/app.module.ts
+++ tpAngulaMaterialUI/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { MaterialModule } from './Material.Module';
+import { MaterialModule } from './material-module';
 import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InputComponent } from './input/input.component';
@@ -6,14 +6,17 @@ import { InputComponent } from './input/input.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AutocompleteComponent } from './component/autocomplete/autocomplete.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent],
+  declarations: [AppComponent, InputComponent, AutocompleteComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- tpAngulaMaterialUI/src/app/material-module.ts
+++ tpAngulaMaterialUI/src/app/material-module.ts
@@ -1,8 +1,9 @@
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  exports: [MatInputModule, MatSelectModule],
+  exports: [MatInputModule, MatSelectModule, MatAutocompleteModule],
 })
 export class MaterialModule {}
