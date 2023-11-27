8b1a1c29ccf5f5e26d3db9ff770bdb6de2593f7e
lina-22 <haquefahinur@gmail.com>
2023-11-27 13:52:14 +0100

    update slider components in angular using material UI components

--- tpAngulaMaterialUI/src/app/component/slider/slider.component.ts
+++ tpAngulaMaterialUI/src/app/component/slider/slider.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
-
+  selectedvalue = 25;
+  startvalue = 30;
+  endvalue = 70;
 }
