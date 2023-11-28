6af14f48a8e9c43f956f831f0422e960b79ccf4f
lina-22 <haquefahinur@gmail.com>
2023-11-28 22:12:08 +0100

    controller || rest api added

---
+++ tp-teams-handling-api/tp-teams-handling-api/src/main/java/fr/sorbonne/paris/nord/university/tpteamshandlingapi/controller/TeamController.java
@@ -0,0 +1,12 @@
+package fr.sorbonne.paris.nord.university.tpteamshandlingapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+    @GetMapping("/hello")
+    public String getTeams() {
+        return "Hello World";
+    }
+}
