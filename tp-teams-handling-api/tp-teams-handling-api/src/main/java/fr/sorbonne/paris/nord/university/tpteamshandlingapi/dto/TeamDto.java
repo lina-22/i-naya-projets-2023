262d04b50c10dd6da473c51773e14b4e6f0624d6
lina-22 <haquefahinur@gmail.com>
2023-11-29 13:41:28 +0100

    dto integrated

---
+++ tp-teams-handling-api/tp-teams-handling-api/src/main/java/fr/sorbonne/paris/nord/university/tpteamshandlingapi/dto/TeamDto.java
@@ -0,0 +1,16 @@
+package fr.sorbonne.paris.nord.university.tpteamshandlingapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamDto {
+    private Long id;
+    private String name;
+    private String slogan;
+}
--- tp-teams-handling-api/tp-teams-handling-api/src/test/java/TeamServiceTest.java
+++ tp-teams-handling-api/tp-teams-handling-api/src/test/java/TeamServiceTest.java
@@ -1,8 +1,17 @@
+import fr.sorbonne.paris.nord.university.tpteamshandlingapi.service.TeamService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class TeamServiceTest {
+    @Mock
+    private TeamService teamService;
+
+    // Given.
+// When.
+// Then.
+
 
 }
